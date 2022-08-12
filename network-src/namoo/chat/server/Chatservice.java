f9b4cac2a963a32152956a7a62400ca2cf1179b9
김양진 <107318969+YANGJIN12@users.noreply.github.com>
2022-08-12 18:06:12 +0900

    Update Chatservice.java

--- network-src/namoo/chat/server/Chatservice.java
+++ network-src/namoo/chat/server/Chatservice.java
@@ -34,6 +34,36 @@ public class Chatservice {
 			tread.start();
 		}
 	}
-	
-	
+	//	접속자 리스트에 접속 클라이언트 추가
+	public void addClient(String nickName, ChatThread thread) {
+		clients.put(nickName, thread);
+	}
+	//	접속자 리스트에서 클라이언트 제거
+	public void removeClient(String nickName) {
+		clients.remove(nickName);
+	}
+	//	모든 접속자에게 메시지를 전송
+	public void sendAllMessage(String message) {
+		Collection<ChatThread> list = clients.values();	
+		Iterator<ChatThread> iter = list.iterator();
+		while (iter.hasNext()) {
+			ChatThread chatThread = (ChatThread) iter.next();
+			chatThread.sendMessage(message);
+		}
+	}
+	//	접속자 닉네임리스트 반환
+	public String getNickNameList(){
+		Set<String> keys = clients.keySet();
+		if(keys.isEmpty()) {
+			return null;
+		}
+		StringBuilder sb = new StringBuilder();
+		Iterator<String> iter = keys.iterator();
+		while (iter.hasNext()) {
+			String nickName = iter.next();
+			sb.append(nickName).append(",");
+			
+		}
+		return sb.toString();		
+	} 
 }
